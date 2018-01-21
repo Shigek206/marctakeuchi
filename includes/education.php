e2b0fa56a570f00ed83fd61704fec517c617955a
marct <marc.takeuchi@gmail.com>
2018-01-21 12:31:29 -0800

    ^removing education years

--- includes/education.php
+++ includes/education.php
@@ -6,18 +6,16 @@
 				<img src="graphics/ico-education.png" alt="Education">
 				<div class="row">
 					<div class="col-xs-12 col-sm-6">
-						<p><strong>Adobe Flex & ColdFusion series,<br />SQL 2008 & Oracle Series</strong><br />
-						UC Irvine Extension, 2007 - 2009</p>
-
 						<p><strong>MBA, Information Systems</strong><br />
-						City University, 2002</p>
+						City University</p>
+						<p><strong>Graduate Certificate, Information Systems</strong><br />
+						City University</p>
 					</div>
 					<div class="col-xs-12 col-sm-6">
-						<p><strong>Graduate Certificate, Information Systems</strong><br />
-						City University, 2000</p>
-
+						<p><strong>Adobe Flex & ColdFusion series,<br />SQL 2008 & Oracle Series</strong><br />
+						UC Irvine Extension</p>
 						<p><strong>BFA, Graphic Media/Design</strong><br />
-						Washington State University (GO COUGS!) 1995</p>
+						Washington State University (GO COUGS!)</p>
 					</div>
 				</div>
 			</div>
