7678d140f4617e2278b291c5f2bcdd979a97d046
marct <marc.takeuchi@gmail.com>
2018-02-10 11:06:10 -0800

    ^updating GA script to new gtag, converting tag into include and adding to all pages, adding return to blog overview link to all blogs

--- blog/post/explore-different-areas-of-web-technology-to-broaden-your-skills.php
+++ blog/post/explore-different-areas-of-web-technology-to-broaden-your-skills.php
@@ -18,19 +18,7 @@
 
 		<link href="../../styles/style.css" rel="stylesheet" type="text/css" media="all" />
 
-		<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-28032204-1']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-
-		</script>
+		<?php include '../../scripts/gtag.php';?>
 	</head>
 
 	<body>
